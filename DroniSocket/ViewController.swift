17ca9683ac1700060f033d1a85a7b7d6780a0b28
gweltaz-calori <andoracontact@gmail.com>
2019-02-09 07:22:03 +0100

    Initial Commit

---
+++ DroniSocket/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DroniSocket
+//
+//  Created by Gweltaz calori on 09/02/2019.
+//  Copyright © 2019 Gweltaz calori. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
